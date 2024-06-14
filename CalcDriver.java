5a116cc39a4a187524d8df295d882a5512e65bf4
RoseMcc04 <mccormacksa@appstate.edu>
2024-06-14 18:44:57 -0400

    decided not to do generics anymore

--- CalcDriver.java
+++ CalcDriver.java
@@ -1,11 +1,4 @@
-public class CalcDriver<T>
+public class CalcDriver() 
 {
-    /**
-     * @param args This is the main method that will execute methods for the
-     * user.
-     */
-    public static void main(String[] args) 
-    {
-
-    }
+    
 }
