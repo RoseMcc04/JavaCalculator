1b1a3362eb451cb2f5b898da4ccb9ed50b176d20
RoseMcc04 <mccormacksa@appstate.edu>
2024-06-14 19:06:40 -0400

    finished the methods I needed to finish

--- Calculator.java
+++ Calculator.java
@@ -19,4 +19,34 @@ public class Calculator
   {
     return n1 - n2;
   }
+
+  /**
+   * @param n1 First integer input
+   * @param n2 Second integer input
+   * @return Returns the product of the two numbers
+   */
+  protected int multiplyNums(int n1, int n2) 
+  {
+    return n1 * n2;
+  }
+
+  /**
+   * @param n1 First integer input
+   * @param n2 Second integer input
+   * @return Returns the quotient of the two numbers
+   */
+  protected int divideNums(int n1, int n2) 
+  {
+    return n1 / n2;
+  }
+
+  /**
+   * @param n1 First integer input
+   * @param n2 Second integer input
+   * @return Returns the remainder of the two numbers
+   */
+  public int modNums(int n1, int n2) 
+  {
+    return n1 % n2;
+  }
 }
