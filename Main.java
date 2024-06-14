1b10f160177c9a2bd6855a0dbb000fd00d029d16
RoseMcc04 <mccormacksa@appstate.edu>
2024-06-14 19:36:24 -0400

    created main method

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
-public class Main {
-    
+public class Main 
+{
+    /**
+     * @param args The main method in this file will execute the calculator 
+     * application for the user.
+     */
+    public static void main(String[] args) 
+    {
+
+    }    
 }
