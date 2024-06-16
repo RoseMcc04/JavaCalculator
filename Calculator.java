0769cb63a0373d3e8d54f344f04cda19ebd38f9d
RoseMcc04 <mccormacksa@appstate.edu>
2024-06-16 11:07:36 -0400

    converted protected methods into public methods

--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,7 @@ public class Calculator
    * @param n2 Second integer input
    * @return Returns the sum of two numbers
    */
-  protected int addNums(int n1, int n2) 
+  public int addNums(int n1, int n2) 
   {
     return n1 + n2;
   }
@@ -17,7 +17,7 @@ public class Calculator
    * @param n2 Second integer input
    * @return Returns the difference of two numbers
    */
-  protected int subtractNums(int n1, int n2) 
+  public int subtractNums(int n1, int n2) 
   {
     return n1 - n2;
   }
@@ -27,7 +27,7 @@ public class Calculator
    * @param n2 Second integer input
    * @return Returns the product of the two numbers
    */
-  protected int multiplyNums(int n1, int n2) 
+  public int multiplyNums(int n1, int n2) 
   {
     return n1 * n2;
   }
@@ -37,7 +37,7 @@ public class Calculator
    * @param n2 Second integer input
    * @return Returns the quotient of the two numbers
    */
-  protected int divideNums(int n1, int n2) 
+  public int divideNums(int n1, int n2) 
   {
     return n1 / n2;
   }
@@ -57,7 +57,7 @@ public class Calculator
    * @param n2 The exponent the user chooses
    * @return Returns the result of the exponent
    */
-  protected int expNums(int n1, int n2) 
+  public int expNums(int n1, int n2) 
   {
     return (int) Math.pow(n1, n2);
   }
