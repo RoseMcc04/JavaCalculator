3ad813fbe3876318864b9dcb31af5eddc808a1c0
RoseMcc04 <mccormacksa@appstate.edu>
2024-06-16 11:06:37 -0400

    practicing inheritance and polymorphism today

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -1,4 +1,11 @@
 public class ScientificCalculator extends Calculator
 {
-    
+    /**
+     * 
+     */
+    @Override
+    protected double addNums(double n1, double n2) 
+    {
+        return n1 + n2;
+    }
 }
