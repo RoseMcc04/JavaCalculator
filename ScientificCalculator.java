b3a524ee8af25ebfa83c838ceee685c4ffec5088
RoseMcc04 <mccormacksa@appstate.edu>
2024-06-16 11:19:00 -0400

    trying to use IllegalArgumentException based on primitive type

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -1,11 +1,23 @@
 public class ScientificCalculator extends Calculator
 {
     /**
-     * 
+     * @param n1 First decimal input
+     * @param n2 Second decimal input
+     * @return Returns a decimal sum of the two inputs
      */
     @Override
-    protected double addNums(double n1, double n2) 
+    public double addNums(double n1, double n2) 
     {
-        return n1 + n2;
+        
+    }
+
+    /**
+     * @param n1 First decimal input
+     * @param n2 Second decimal input
+     * @return Returns the difference of the inputs
+     */
+    public double substractNums(double n1, double n2) 
+    {
+        return n1 - n2;
     }
 }
