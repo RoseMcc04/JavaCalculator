59db0100ddb3e17d97de7f25c4cd99f3803a170d
RoseMcc04 <mccormacksa@appstate.edu>
2024-06-29 14:03:39 -0400

    wrong branch, oops

--- CalculatorOperations.java
+++ CalculatorOperations.java
@@ -4,29 +4,35 @@ public interface CalculatorOperations<T>
    * @param n1 First input
    * @param n2 Second input
    */
-  public void add(T n1, T n2);
+  public T add(T n1, T n2);
 
   /**
    * @param n1 First input
    * @param n2 Second input
    */
-  public void subtract(T n1, T n2);
+  public T subtract(T n1, T n2);
 
   /**
    * @param n1 First input
    * @param n2 Second input
    */
-  public void multiply(T n1, T n2);
+  public T multiply(T n1, T n2);
 
   /**
    * @param n1 First input
    * @param n2 Second input
    */
-  public void divide(T n1, T n2);
+  public T divide(T n1, T n2);
 
   /**
    * @param n1 First input
    * @param n2 Second input
    */
-  public void modulus(T n1, T n2); 
+  public T modulus(T n1, T n2);
+  
+  /**
+   * @param n1 First input
+   * @param n2 Second input
+   */
+  public T exponent(T n1, T n2);
 }
