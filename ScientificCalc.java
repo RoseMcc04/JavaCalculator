72e16a0ae60111fa55b9395007ae7125bdce18fc
RoseMcc04 <mccormacksa@appstate.edu>
2024-06-29 18:27:53 -0400

    testing out a generic interface

--- ScientificCalc.java
+++ ScientificCalc.java
@@ -1,4 +1,13 @@
-public class ScientificCalc implements CalculatorOperations<> extends DecimalCalc
+public class ScientificCalc implements CalculatorOperations<Double>
 {
-    
+    /**
+     * @param n1 First input
+     * @param n2 Second input
+     * @return Returns sum of the inputs
+     */
+    @Override
+    public Double add(Double n1, Double n2) 
+    {
+        return n1 + n2;
+    }
 }
