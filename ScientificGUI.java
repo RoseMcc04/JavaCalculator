251d1b651625c010d18c5ed1bc02e4c67e7636eb
RoseMcc04 <mccormacka@appstate.edu>
2024-07-11 15:37:15 -0400

    added class declaration to ScientificGUI.java

---
+++ ScientificGUI.java
@@ -0,0 +1,11 @@
+// Import statement that imports all javax.swing objects
+import javax.swing.*;
+
+/**
+ * @author Rose McCormack
+ * @version 2024
+ */
+public class ScientificGUI extends ScientificCalc 
+{
+    
+}
