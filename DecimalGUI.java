d95df43a118dfd58aac35b9802438878bec662a2
RoseMcc04 <mccormacka@appstate.edu>
2024-07-11 15:39:18 -0400

    added class declaration to DecimalGui.java

---
+++ DecimalGUI.java
@@ -0,0 +1,11 @@
+// Import statement that import all javax.swing objects
+import javax.swing.*;
+
+/**
+ * @author Rose McCormack
+ * @version 2024
+ */
+public class DecimalGUI extends DecimalCalc
+{
+    
+}
