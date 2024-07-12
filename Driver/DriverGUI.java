1f57371d0df73278bac67226b74e0b134b228cce
RoseMcc04 <mccormacka@appstate.edu>
2024-07-12 09:18:17 -0400

    created helper methods to activate secondary GUIs

--- Driver/DriverGUI.java
+++ Driver/DriverGUI.java
@@ -11,5 +11,43 @@ import Calculator.ScientificGUI;
  */
 public class DriverGUI 
 {
-    
+    /**
+     * @param args This is the main method that will activate the primary GUI
+     * for user input and then activate a secondary GUI based on user 
+     * preference.
+     */
+    public static void main(String[] args) 
+    {
+
+    }
+
+    /**
+     * This method will allow us to include a DecimalGUI object in the primary 
+     * DriverGUI.
+     */
+    @SuppressWarnings("unused")
+    private void CallDecimalGUI() 
+    {
+        DecimalGUI newGUI = new DecimalGUI();
+    }
+
+    /**
+     * This method will allow us to include an IntegerGUI object in the 
+     * primary DriverGUI.
+     */
+    @SuppressWarnings("unused")
+    private void CallIntegerGUI() 
+    {
+        IntegerGUI newGUI = new IntegerGUI();
+    }
+
+    /**
+     * This method will allow us to include a ScientificGUI oject in the
+     * primary DriverGUI.
+     */
+    @SuppressWarnings("unused")
+    private void CallScientificGUI() 
+    {
+        ScientificGUI newGUI = new ScientificGUI();
+    }
 }
