51504421f23f972f8a4a7a780bf52dc4a48e0b1d
RoseMcc04 <mccormacka@appstate.edu>
2024-07-11 21:48:43 -0400

    added ScientificGUI to GUIs folder

---
+++ GUIs/ScientificGUI.java
@@ -0,0 +1,14 @@
+package GUIs;
+// Import statement that imports all javax.swing objects
+import javax.swing.*;
+
+import ScientificCalc;
+
+/**
+ * @author Rose McCormack
+ * @version 2024
+ */
+public class ScientificGUI extends ScientificCalc 
+{
+    
+}
